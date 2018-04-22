2bf462ecde49042e01126840756b3ef133cec9fd
Leonid <moryakovl@gmail.com>
2018-04-22 14:40:48 +0300

    added the short id for Stars functional component

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -8,8 +8,7 @@ function Stars({count}) {
 
   let starList = new Array(count).fill(0);
 
-  //shortid will be best for key
-  starList = starList.map((el, index) => <li key={index}><Star/></li> );
+  starList = starList.map((el, index) => <li key={shortid.generate()}><Star/></li> );
 
   return (
     <ul className="card-body-stars u-clearfix">
